276810ce203b62d441ee183436dca5861938f912
Tam Le <philosocode@gmail.com>
2021-07-21 19:11:57 -0600

    Begin working on consume workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import { ConsumePage } from "consume/pages/consume.page";
 
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={ConsumePage} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/consume/pages/consume.page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Heading } from "typography/components/heading.component";
+
+export const ConsumePage: React.FC = () => {
+  return (
+    <div>
+      <Heading element="h1" type="title">
+        Consume Workflow
+      </Heading>
+    </div>
+  );
+};
