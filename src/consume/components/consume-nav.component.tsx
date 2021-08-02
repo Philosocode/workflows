2bc079c960a403be9d41cb910ad0079e155c2f4f
Tam Le <philosocode@gmail.com>
2021-08-02 15:01:32 -0600

    Add button to reset workflow

--- src/consume/components/consume-nav.component.tsx
+++ src/consume/components/consume-nav.component.tsx
@@ -1,12 +1,14 @@
 import { HStack } from "@chakra-ui/react";
 
+import { useSelector } from "react-redux";
+import { selectStep } from "consume/redux/consume.selectors";
+
+import { ConsumeSettings } from "./consume-settings.component";
+import { ConsumeResetModal } from "./consume-reset-modal.component";
 import { GetUnstuckModal } from "modal/components/get-unstuck-modal.component";
 import { RandoHookModal } from "modal/components/rando-hook-modal.component";
 import { StudyBlockCounter } from "consume/components/study-block-counter.component";
 import { ToggleThemeButton } from "./toggle-theme-button.component";
-import { ConsumeSettings } from "./consume-settings.component";
-import { useSelector } from "react-redux";
-import { selectStep } from "consume/redux/consume.selectors";
 
 export function ConsumeNav() {
   const step = useSelector(selectStep);
@@ -16,9 +18,10 @@ export function ConsumeNav() {
       <HStack>
         <RandoHookModal />
         <GetUnstuckModal />
+        {step > 1 && <ConsumeResetModal />}
       </HStack>
       <HStack>
-        {step !== 1 && <ConsumeSettings />}
+        {step > 1 && <ConsumeSettings />}
         <ToggleThemeButton />
         <StudyBlockCounter />
       </HStack>
