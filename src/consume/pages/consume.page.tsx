cd2f281545237fbab80b2beb4f8094db1abcfc34
Tam Le <philosocode@gmail.com>
2021-08-03 07:43:51 -0600

    Use hook for updating page param

--- src/consume/pages/consume.page.tsx
+++ src/consume/pages/consume.page.tsx
@@ -1,33 +1,14 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-
 import { useAppDispatch } from "shared/redux/store";
 import { setStep } from "consume/redux/consume.slice";
-import { consumeRoutes } from "consume/routes/consume.routes";
 
 import { ConsumeNav } from "consume/components/consume-nav.component";
+import { usePageParam } from "shared/hooks/use-page-param.hook";
 
 export function ConsumePage() {
-  const params = useParams<{ pageNumber: string }>();
   const dispatch = useAppDispatch();
 
   // whenever page number param changes, set it
-  useEffect(() => {
-    if (params.pageNumber) {
-      const pageParam = Number.parseInt(params.pageNumber);
-      let pageNumber = pageParam;
-
-      if (
-        typeof pageNumber !== "number" ||
-        pageNumber < 1 ||
-        pageNumber > consumeRoutes.length + 1
-      ) {
-        pageNumber = 1;
-      }
-
-      dispatch(setStep(pageNumber));
-    }
-  }, [dispatch, params]);
+  usePageParam((pageNumber) => dispatch(setStep(pageNumber)));
 
   return <ConsumeNav />;
 }
