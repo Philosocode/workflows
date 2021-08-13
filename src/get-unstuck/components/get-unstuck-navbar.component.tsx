fc169d636149b5c1c3673068117ed9d420bac4ce
Tam Le <philosocode@gmail.com>
2021-08-12 18:27:38 -0600

    Sort imports

--- src/get-unstuck/components/get-unstuck-navbar.component.tsx
+++ src/get-unstuck/components/get-unstuck-navbar.component.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
+import { IoMdReturnLeft } from "react-icons/io";
+
 import { useAppSelector } from "shared/redux/store";
 import { selectRedirectUrl } from "step/step.slice";
 
 import { AppNavbar } from "navbar/components/app-navbar.component";
 import { ToggleThemeButton } from "navbar/components/toggle-theme-button.component";
-import { IoMdReturnLeft } from "react-icons/io";
 import { IconButton } from "shared/components/icon-button.component";
-import { Link } from "react-router-dom";
 
 export function GetUnstuckNavbar() {
   const redirectUrl = useAppSelector(selectRedirectUrl);
